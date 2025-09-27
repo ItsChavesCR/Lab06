dddd9c23822d216219189cf84c7d2aeb5045471f
ItsChavesCR <achaba03x@gmail.com>
2025-09-27 10:01:55 -0600

    fix: update redirect URI and base URL to use production environment

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ const config = {
   authRequired: false,
   auth0Logout: true,
   secret: SECRET,
-  baseURL: 'http://localhost:3000',
+  baseURL: 'https://lab06-azsc.onrender.com',
   clientID: OKTA_CLIENT_ID,
   issuerBaseURL: OKTA_ISSUER_URI
 };
@@ -34,7 +34,7 @@ let oidc = new ExpressOIDC({
   client_id: OKTA_CLIENT_ID,
   client_secret: OKTA_CLIENT_SECRET,
   redirect_uri: REDIRECT_URI,
-  routes: { callback: { defaultRedirect: "http://localhost:3000/dashboard" } },
+  routes: { callback: { defaultRedirect: "https://lab06-azsc.onrender.com/dashboard" } },
   scope: 'openid profile'
 });
 
